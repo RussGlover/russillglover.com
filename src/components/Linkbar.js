d8fb6d5691fc5ef3b3a50822a70d7fc2c16e4d6a
Russill Glover <hello@russillglover.com>
2019-12-14 18:12:12 -0500

    - Initial commit

---
+++ src/components/Linkbar.js
@@ -0,0 +1,34 @@
+import Link from "./Link";
+import React from "react";
+import styled from "styled-components";
+const Ul = styled.ul`
+  list-style-type: none;
+  border-right: 2px ridge #8ef9f3;
+  margin: 13px 0;
+  padding-right: 30px;
+`;
+
+const Li = styled.li`
+  margin: 8px 0;
+`;
+
+const Linkbar = () => (
+  <nav>
+    <Ul>
+      <Li>
+        <Link to="/">Home</Link>
+      </Li>
+      <Li>
+        <Link to="/projects">Projects</Link>
+      </Li>
+      <Li>
+        <Link to="https://github.com/RussGlover">Github</Link>
+      </Li>
+      <Li>
+        <Link to="mailto:hello@russillglover.com">Email</Link>
+      </Li>
+    </Ul>
+  </nav>
+);
+
+export default Linkbar;
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,29 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components";
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
+import Linkbar from "./Linkbar";
 
-import Header from "./header"
-import "./layout.css"
+const Home = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: -1;
+`;
+
+const Main = styled.main`
+  flex: start;
+  text-align: start;
+  padding-left: 8px;
+`;
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,32 +38,25 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <Home>
+        <Linkbar />
+        <Main>{children}</Main>
+      </Home>
+      {/* <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer> */}
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
