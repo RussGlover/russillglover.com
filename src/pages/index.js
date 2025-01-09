69792f19d19a5d009aa223638bd66129c03b8e0d
Russill Glover <hello@russillglover.com>
2025-01-09 12:15:38 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Russill Glover</h1>
-    <p>Full time computer engineer, part time cat dad.</p>
+    <p>Senior Software Engineer. Maximizing productivity for Frontend Developers at Stripe.</p>
     {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div> */}
   </Layout>
 );
